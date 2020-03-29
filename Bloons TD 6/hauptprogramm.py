9e463f0eab0a734cf288c90f0482931c613d9e32
Deniz Schmidt <noahdenizschmidt@outlook.de>
2020-03-29 19:45:45 +0200

    Anfang von Knöpfen mit Dictionary

--- Bloons TD 6/hauptprogramm.py
+++ Bloons TD 6/hauptprogramm.py
@@ -1,6 +1,6 @@
 import pygame
 from os import path
-from sprite_arten import *
+import pdb
 
 # Konstante Variablen
 BREITE = 1920
@@ -13,6 +13,70 @@ WEISS = (255, 255, 255)
 ROT = (255, 0, 0)
 GRUEN = (0, 255, 0)
 
+# breakpoint()
+
+class Knopf(pygame.sprite.Sprite):
+    def __init__(self, konfiguration):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(path.join(extras_ordner, konfiguration["datei"])).convert()
+        self.image.set_colorkey(konfiguration["colorkey"])
+        self.rect = self.image.get_rect()
+        self.rect.topleft = konfiguration["position"]
+        self.aktion = konfiguration["aktion"]
+        alle_figuren.add(self)
+        alle_knoepfe.add(self)
+
+
+class Charakter(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+
+class Pfeil(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+
+class Bloons(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+
+class Map(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+
+class Pop(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+
+class Punkt(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.rect = pygame.Rect(x, y, x + 1, y + 1)
+
+
+knopf_konfiguration = {
+    "startbildschirm": [
+        {
+            "datei": "play button.png",
+            "colorkey": WEISS,
+            "position": (760, 934),
+            "aktion": "Gehe zu Hauptmenue"
+        }
+    ],
+    "hauptmenue": [
+        {
+            "datei": "play auf hauptmenue.png",
+            "colorkey": WEISS,
+            "position": (760, 910),
+            "aktion": "zeige map an"
+        }
+    ]
+}
+
 
 pygame.init()
 bildschirm = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
@@ -20,15 +84,21 @@ pygame.display.set_caption("Bloons TD 6")
 uhr = pygame.time.Clock()
 
 alle_figuren = pygame.sprite.Group()
+alle_knoepfe = pygame.sprite.Group()
+
 spiel_ordner = path.dirname(__file__)
 bilder_ordner = path.join(spiel_ordner, "Bilder")
 extras_ordner = path.join(bilder_ordner, "Extras")
+
 startbildschirm = pygame.image.load(path.join(extras_ordner, "start.jpg")).convert()
-play_button = pygame.image.load(path.join(extras_ordner, "play button.png")).convert()
-play_button.get_rect()
-allstart = pygame.image.load(path.join(extras_ordner, "AllstartBTD6.png")).convert()
-play_button.set_colorkey(WEISS)
 hintergrund = startbildschirm
+
+for knopf in knopf_konfiguration["startbildschirm"]:
+    Knopf(knopf)
+
+
+allstart = pygame.image.load(path.join(extras_ordner, "AllstartBTD6.png")).convert()
+
 
 # Game Loop
 laufend = True
@@ -39,8 +109,14 @@ while laufend:
 
     for ereignis in pygame.event.get():
         if ereignis.type == pygame.MOUSEBUTTONDOWN:
+            # breakpoint()
             maus_pos = pygame.mouse.get_pos()
-            if play_button.rect.collidepoint(maus_pos):
+            print(maus_pos)
+            geklickte_knoepfe = pygame.sprite.spritecollide(
+                Punkt(maus_pos[0], maus_pos[1]), alle_knoepfe, False
+            )
+            print(geklickte_knoepfe[0].aktion)
+            if geklickte_knoepfe[0].aktion == "Gehe zu Hauptmenue":
                 hintergrund = allstart
 
         # für Fenster schließen checken
@@ -51,7 +127,6 @@ while laufend:
     # Malen
     bildschirm.fill(SCHWARZ)
     bildschirm.blit(hintergrund, (0, 0))
-    bildschirm.blit(play_button, (760, 934))
     alle_figuren.draw(bildschirm)
     # Bildschirm umdrehen
     pygame.display.flip()
